105a7ea0ae63f5162dfbbc80d59ac7d914367643
Mark Wotton <mwotton@gmail.com>
2014-12-13 20:12:52 +0700

    showing off

--- jimmy.hs
+++ jimmy.hs
@@ -1,22 +1,22 @@
-import           Control.Applicative ((<$>))
+{-# LANGUAGE BangPatterns #-}
+import           Control.Applicative ((<$>), (<*>))
 import           Control.Arrow       ((&&&))
 import           Data.Char           (isUpper, toLower)
 import           Data.List           (group, sort)
-import qualified Data.Map.Strict     as Map
+import           Data.Map.Strict     (delete, fromList, insert, lookup, null)
+import           Prelude             hiding (lookup, null)
 import           System.Environment  (getArgs)
 
-main = do
-  letters <- concat <$> getArgs
-  dict <- map (map toLower) . filter (\x -> isUpper (head x) && length x >=3  ) . lines
-          <$> readFile "/usr/share/dict/words"
-  putStr . unlines . map unwords . search dict $
-    (Map.fromList . map (head &&& length) . group $ sort letters)
+main = (search <$> (buildWords <$> readFile "/usr/share/dict/words") <*> (buildInventory <$> getArgs))
+            >>= putStr . unlines . map unwords
+  where buildInventory = fromList . map (head &&& length) . group . sort . concat
+        buildWords = map (map toLower) . filter (\x -> isUpper (head x) && length x >=3) . lines
 
 search [] _ = []
-search (w:wordlist) inventory = maybe rest continue (fits w inventory)
+search orig@(w:wordlist) inventory = maybe rest continue (fits w inventory)
   where rest = search wordlist inventory
-        continue i = if Map.null i then [[w]] else (map (w:) (search (w:wordlist) i)) ++ rest
+        continue i = if null i then [[w]] else map (w:) (search orig i) ++ rest
 
-fits word inventory = foldr  get (Just inventory) word
-  where get letter store = store >>= \i -> Map.lookup letter i >>= \n ->
-          return $ (if n == 1 then Map.delete else Map.adjust (+ (-1))) letter i
+fits (!word) (!inventory) = foldr  get (Just inventory) word
+  where get letter store = store >>= \i -> lookup letter i >>= \n ->
+          return $ (if n == 1 then delete else flip insert (n-1)) letter i
