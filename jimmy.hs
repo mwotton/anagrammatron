ecd349aacd28cf085c1dbddb5b09d48548e9b03b
Mark Wotton <mwotton@gmail.com>
2014-12-13 07:13:17 +0700

    gordonified

--- jimmy.hs
+++ jimmy.hs
@@ -1,34 +1,22 @@
-
-import           Control.Applicative   ((<$>))
-import           Control.Arrow         ((&&&))
-import qualified Data.ByteString.Char8 as BS
-import           Data.Char             (toLower)
-import           Data.Char             (isUpper)
-import           Data.List
-import qualified Data.Map              as Map
-import           System.Environment    (getArgs)
+import           Control.Applicative ((<$>))
+import           Control.Arrow       ((&&&))
+import           Data.Char           (isUpper, toLower)
+import           Data.List           (group, sort)
+import qualified Data.Map.Strict     as Map
+import           System.Environment  (getArgs)
 
 main = do
   letters <- concat <$> getArgs
-  dict <- map (map toLower) . filter (\x -> isUpper (head x) && length x >=3  ) . lines <$> readFile "/usr/share/dict/words"
-  putStr $ unlines $ map unwords $ search dict (inventory letters)
+  dict <- map (map toLower) . filter (\x -> isUpper (head x) && length x >=3  ) . lines
+          <$> readFile "/usr/share/dict/words"
+  putStr . unlines . map unwords . search dict $
+    (Map.fromList . map (head &&& length) . group $ sort letters)
 
-inventory = Map.fromList . map (head &&& length) . group . sort
-
-search :: [String] -> Map.Map Char Int -> [[String]]
 search [] _ = []
-search (w:wordlist) inventory = case fits w inventory  of
-  Nothing -> rest
-  Just i -> if Map.null i
-            then [[w]]
-            else  (map (w:) (search (w:wordlist) i)) ++ rest
+search (w:wordlist) inventory = maybe rest continue (fits w inventory)
   where rest = search wordlist inventory
+        continue i = if Map.null i then [[w]] else (map (w:) (search (w:wordlist) i)) ++ rest
 
-fits :: String -> Map.Map Char Int -> Maybe (Map.Map Char Int)
 fits word inventory = foldr  get (Just inventory) word
-  where get _ Nothing = Nothing
-        get letter (Just i) =  case Map.lookup letter i of
-          Nothing -> Nothing
-          Just n -> case n of
-            1 -> Just (Map.delete letter i)
-            n -> Just (Map.adjust (+ (-1)) letter i)
+  where get letter store = store >>= \i -> Map.lookup letter i >>= \n ->
+          return $ (if n == 1 then Map.delete else Map.adjust (+ (-1))) letter i
