aa50580c84aea58bad48aa118a0089acf81eeb6a
Mark Wotton <mwotton@gmail.com>
2014-12-14 10:32:01 +0700

    preformat

--- jimmy.hs
+++ jimmy.hs
@@ -2,21 +2,51 @@
 import           Control.Applicative ((<$>), (<*>))
 import           Control.Arrow       ((&&&))
 import           Data.Char           (isUpper, toLower)
+import           Data.Char           (isAlpha)
 import           Data.List           (group, sort)
-import           Data.Map.Strict     (delete, fromList, insert, lookup, null)
+import           Data.Map            (lookup)
+import           Data.Map            (fromList, keys)
+import           Data.Maybe          (fromJust)
+
+import           Data.Char           (isAscii)
+import           Data.Int            (Int64)
+import           Data.Map            (toList)
+import           Data.Map            (fromListWith)
+import           Debug.Trace         (trace)
 import           Prelude             hiding (lookup, null)
 import           System.Environment  (getArgs)
 
-main = (search <$> (buildWords <$> readFile "/usr/share/dict/words") <*> (buildInventory <$> getArgs))
-            >>= putStr . unlines . map unwords
-  where buildInventory = fromList . map (head &&& length) . group . sort . concat
-        buildWords = map (map toLower) . filter (\x -> isUpper (head x) && length x >=3) . lines
+main = (search <$> (buildWords . lines <$> readFile "/usr/share/dict/words")
+               <*> (buildInventory <$> getArgs))
+            >>= print -- putStr . unlines . map unwords
 
+buildWords :: [String] -> [(Int64, [String])]
+buildWords l = let res = [(sig x,[x]) | r <- l, isUpper (head r), all isAlpha r, length r >= 3,
+                          let x = map toLower r]
+               in toList $ fromListWith (++) res
+--buildWords =  map ((\x -> (sig x,x)) . map toLower) . filter (\x -> isUpper
+--                          (head x) && length x >=3 && all isAlpha x) . lines
+
+buildInventory :: [String] -> Int64
+buildInventory = sig . concat
+
+
+search :: [(Int64, [String])] -> Int64 -> [[[String]]]
 search [] _ = []
-search orig@(w:wordlist) inventory = maybe rest continue (fits w inventory)
+search orig@((sigw,w):wordlist) inventory = maybe rest continue (numfits sigw inventory)
   where rest = search wordlist inventory
-        continue i = if null i then [[w]] else map (w:) (search orig i) ++ rest
+        continue i = if i==1 then [[w]] else map (w:) (search orig i)  ++ rest
 
-fits (!word) (!inventory) = foldr  get (Just inventory) word
-  where get letter store = store >>= \i -> lookup letter i >>= \n ->
-          return $ (if n == 1 then delete else flip insert (n-1)) letter i
+numfits !s !inventory = case inventory `quotRem` s of
+  (!n,!0) -> Just n
+  _ -> Nothing
+
+
+
+primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101]
+
+
+sig  = product . map (\(c,n) ->  (fromJust $ lookup c costs) ^ n ) . lettercount
+costs = fromList $ zip ['a' .. 'z'] primes
+
+lettercount = map (head &&& length ) . group . sort . filter isAscii . map toLower
