31224b479c7421e3ef13d6f36210d743cf6fbac0
Mark Wotton <mwotton@gmail.com>
2014-12-14 12:02:01 +0700

    vectorify

--- jimmy.hs
+++ jimmy.hs
@@ -1,48 +1,51 @@
 {-# LANGUAGE BangPatterns #-}
 import           Control.Applicative   ((<$>), (<*>))
 import           Control.Arrow         ((&&&))
-import           Data.Char             (isUpper, toLower)
-import           Data.Char             (isAlpha)
-import           Data.List             (group, sort)
-import           Data.Map              (lookup)
-import           Data.Map              (fromList, keys)
-import           Data.Maybe            (fromJust)
-
 import qualified Data.ByteString.Char8 as BS
-import           Data.Char             (isAscii)
+import           Data.Char             (isAlpha, isAscii, isUpper, toLower)
 import           Data.Int              (Int64)
-import           Data.Map              (toList)
-import           Data.Map              (fromListWith)
-import           Debug.Trace           (trace)
+import           Data.List             (group, sort)
+import           Data.Map              (fromList, lookup)
+import           Data.Maybe            (fromJust)
+import           Data.Vector           (Vector, (!))
+import qualified Data.Vector           as V
 import           Prelude               hiding (lookup, null)
 import           System.Environment    (getArgs)
 
-
+main :: IO ()
 main = (search <$> (buildWords . BS.lines <$> BS.readFile "/usr/share/dict/words")
                <*> (buildInventory <$> getArgs))
             >>= BS.putStr . BS.unlines . map BS.unwords
 
-buildWords :: [BS.ByteString] -> [(Int64, BS.ByteString)]
-buildWords l =[(bsSig x,x) | r <- l, isUpper (BS.head r), BS.all isAlpha r, BS.length r >= 3,
+buildWords :: [BS.ByteString] -> Vector (Int64, BS.ByteString)
+buildWords l = V.fromList [(bsSig x,x) | r <- l, isUpper (BS.head r), BS.all isAlpha r, BS.length r >= 3,
                let x = BS.map toLower r]
 
 buildInventory :: [String] -> Int64
 buildInventory = sig . concat
 
-search :: [(Int64, BS.ByteString)] -> Int64 -> [[BS.ByteString]]
-search [] _ = []
-search orig@((sigw,w):wordlist) !inventory = case inventory `quotRem` sigw of
-  (n,0) -> if n == 1 then [[w]] else rest ++ map (w:) (search orig n)
-  _ -> rest
-  where rest = search wordlist inventory
+search :: Vector (Int64, BS.ByteString) -> Int64 -> [[BS.ByteString]]
+search v invent = go (V.length v - 1) invent where
+  go (-1) _ = []
+  go index !inventory =
+    let (sigw,w) = v ! index in
+    case inventory `quotRem` sigw of
+      (n,0) -> if n == 1 then [[w]] else rest ++ map (w:) (go (index-1) n)
+      _ -> rest
+    where rest = go (index - 1) inventory
 
+primes :: [Int64]
 primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101]
 
+sig :: String -> Int64
 sig  = product . map (\(c,n) ->  (fromJust $ lookup c costs) ^ n ) . lettercount
 
+bsSig :: BS.ByteString -> Int64
 bsSig = BS.foldl' (\a c -> fromJust (lookup c costs) * a) 1
 costs = fromList $ zip letters primes
 
+lettercount :: String -> [(Char, Int)]
 lettercount = map (head &&& length ) . group . sort . filter isAscii . map toLower
 
+letters :: String
 letters = map toLower "ETAONRISHDLFCMUGYPWBVKJXQZ"
