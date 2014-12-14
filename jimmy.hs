4194b801cde3e65957ac53667774c301c6235c67
Mark Wotton <mwotton@gmail.com>
2014-12-14 11:28:14 +0700

    use bytestrings

--- jimmy.hs
+++ jimmy.hs
@@ -1,39 +1,36 @@
 {-# LANGUAGE BangPatterns #-}
-import           Control.Applicative ((<$>), (<*>))
-import           Control.Arrow       ((&&&))
-import           Data.Char           (isUpper, toLower)
-import           Data.Char           (isAlpha)
-import           Data.List           (group, sort)
-import           Data.Map            (lookup)
-import           Data.Map            (fromList, keys)
-import           Data.Maybe          (fromJust)
+import           Control.Applicative   ((<$>), (<*>))
+import           Control.Arrow         ((&&&))
+import           Data.Char             (isUpper, toLower)
+import           Data.Char             (isAlpha)
+import           Data.List             (group, sort)
+import           Data.Map              (lookup)
+import           Data.Map              (fromList, keys)
+import           Data.Maybe            (fromJust)
 
-import qualified Data.ByteString     as BS
-import           Data.Char           (isAscii)
-import           Data.Int            (Int64)
-import           Data.Map            (toList)
-import           Data.Map            (fromListWith)
-import           Debug.Trace         (trace)
-import           Prelude             hiding (lookup, null)
-import           System.Environment  (getArgs)
+import qualified Data.ByteString.Char8 as BS
+import           Data.Char             (isAscii)
+import           Data.Int              (Int64)
+import           Data.Map              (toList)
+import           Data.Map              (fromListWith)
+import           Debug.Trace           (trace)
+import           Prelude               hiding (lookup, null)
+import           System.Environment    (getArgs)
 
 
-main = (search <$> (buildWords . lines <$> readFile "/usr/share/dict/words")
-                 <*> (buildInventory <$> getArgs))
-            >>= putStr . unlines . map unwords
+main = (search <$> (buildWords . BS.lines <$> BS.readFile "/usr/share/dict/words")
+               <*> (buildInventory <$> getArgs))
+            >>= BS.putStr . BS.unlines . map BS.unwords
 
-buildWords :: [String] -> [(Int64, String)]
-buildWords l =[(sig x,x) | r <- l, isUpper (head r), all isAlpha r, length r >= 3,
-               let x = map toLower r]
---               in toList $ fromListWith (++) res
---buildWords =  map ((\x -> (sig x,x)) . map toLower) . filter (\x -> isUpper
---                          (head x) && length x >=3 && all isAlpha x) . lines
+buildWords :: [BS.ByteString] -> [(Int64, BS.ByteString)]
+buildWords l =[(bsSig x,x) | r <- l, isUpper (BS.head r), BS.all isAlpha r, BS.length r >= 3,
+               let x = BS.map toLower r]
 
 buildInventory :: [String] -> Int64
 buildInventory = sig . concat
 
 
-search :: [(Int64, String)] -> Int64 -> [[String]]
+search :: [(Int64, BS.ByteString)] -> Int64 -> [[BS.ByteString]]
 search [] _ = []
 search orig@((sigw,w):wordlist) !inventory = case inventory `quotRem` sigw of
   (n,0) -> if n == 1 then [[w]] else rest ++ map (w:) (search orig n)
@@ -45,11 +42,11 @@ primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
 
 
 sig  = product . map (\(c,n) ->  (fromJust $ lookup c costs) ^ n ) . lettercount
+
+
+bsSig = BS.foldl' (\a c -> fromJust (lookup c costs) * a) 1
 costs = fromList $ zip letters primes
 
 lettercount = map (head &&& length ) . group . sort . filter isAscii . map toLower
 
-
--- we use
---letters = ['a' .. 'z']
 letters = map toLower "ETAONRISHDLFCMUGYPWBVKJXQZ"
